8bbec5c85b09a721ae08c259d9a4397801c85264
Luiz Felipe de J. Santos <luiz_felipe_js@hotmail.com>
2022-02-07 17:04:08 -0300

    Errasing Comments

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -1,28 +1,9 @@
-// import { useState, useEffect, useContext } from "react";
-// import { getToday, markHabit } from "../../services/mywallet";
 import { BiLogOut, BiPlusCircle, BiMinusCircle,  } from "react-icons/bi";
 
 import { Container, Statement, Footer, Icon, StyledLink1, StyledLink2, Header } from "./WalletStyle";
-// import UserContext from "../../contexts/UserContext";
+
 
 export default function Wallet() {
-  // const [deposit, setDeposit] = useState(null);
-  // //  const [withdraw, setWithdraw] = useState(null);
-  // const { token } = useContext(UserContext);
-  // const auth = { headers: { Authorization: `Bearer ${token}` } };
-
-  // useEffect(() => {
-  //   renderHomePage();
-  // }, []);
-
-  // function renderHomePage() {
-  //   const promise = getToday(auth);
-  //   promise.then((response) => {
-  //     setDeposit(response.data);
-
-  //     promise.catch((error) => console.log(error.response));
-  //   });
-  // }
 
   return (
     <>
